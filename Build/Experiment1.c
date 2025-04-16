8435a796cfca868764dee1841be1bc7a6a023465
Ahmik Muhammad <ahmik.muhammad@stonybrook.edu>
2025-04-16 12:55:25 -0400

    experiment 1

--- Build/Experiment1.c
+++ Build/Experiment1.c
@@ -2,12 +2,29 @@
 #include <string.h>
 
 int main() {
-    char str[8];
+    char str[20];
     printf("Enter a word: ");
     scanf("%s", str);
 
     int len = strlen(str);
     int isPalindrome = 1;
+    
+    // convert to lowercase
+    // for (int i = 0; i < len / 2; i++) {
+    //     if ('A' <= str[i] <= 'Z') {
+    //         str[i] = str[i] - 'A';
+    //     }
+    // }
+    char lowercase(char ch) {
+        if ('A' <= ch && ch <= 'Z') {
+            return ch + 32; // convert to lowercase
+        }
+        return ch;
+    }
+
+    for (int i = 0; i < len; i++) {
+        str[i] = lowerCase(str[i]);
+    }
 
     for (int i = 0; i < len / 2; i++) {
         if (str[i] != str[len - 1 - i]) {
@@ -15,7 +32,7 @@ int main() {
             break;
         }
     }
-
+    
     if (isPalindrome) {
         printf("It's a palindrome!\n");
     } else {
